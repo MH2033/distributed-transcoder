21af2fea2f38beb03c309b4508179bbf0388a590
MH2033 <mh.akbare@gmail.com>
2022-04-10 16:31:38 +0430

    Add encode type enum

--- include/transcoder.hpp
+++ include/transcoder.hpp
@@ -6,5 +6,12 @@
 #include <gst/app/app.h>
 #include <iostream>
 
-void transcode(std::string input_file_name, std::string output_file_name);
+enum class EncodeType{
+  H264,
+  H265,
+  VP8,
+  VP9
+};
+
+void transcode(std::string input_file_name, std::string output_file_name, EncodeType encode_type);
 #endif 
